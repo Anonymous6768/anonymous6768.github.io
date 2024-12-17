e7dfbaa794de7c994d2ef2b21ba8a65910701aff
Anonymous6565 <cwmaender@gmail.com>
2024-12-16 21:20:40 -0500

    Update server.js

--- chat/server.js
+++ chat/server.js
@@ -1,9 +1,36 @@
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "https://chokitu.github.io",
-        methods: ["GET", "POST"]
-    }
-});
+const http = require('http');
+const ws = require('ws');
 
-app.set('port', process.env.PORT || 3000);
-server.listen(port); 
+const wss = new ws.Server({noServer: true});
+
+function accept(req, res) {
+  // all incoming requests must be websockets
+  if (!req.headers.upgrade || req.headers.upgrade.toLowerCase() != 'websocket') {
+    res.end();
+    return;
+  }
+
+  // can be Connection: keep-alive, Upgrade
+  if (!req.headers.connection.match(/\bupgrade\b/i)) {
+    res.end();
+    return;
+  }
+
+  wss.handleUpgrade(req, req.socket, Buffer.alloc(0), onConnect);
+}
+
+function onConnect(ws) {
+  ws.on('message', function (message) {
+    message = message.toString();
+    let name = message.match(/([\p{Alpha}\p{M}\p{Nd}\p{Pc}\p{Join_C}]+)$/gu) || "Guest";
+    ws.send(`Hello from server, ${name}!`);
+
+    setTimeout(() => ws.close(1000, "Bye!"), 5000);
+  });
+}
+
+if (!module.parent) {
+  http.createServer(accept).listen(8080);
+} else {
+  exports.accept = accept;
+}
